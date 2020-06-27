f3aaeb4570ac9017a0e42e330ae02acfe29dfd37
Dhivya Janakiraman <djanakiraman@vera.com>
2020-06-26 22:21:35 -0700

    Initial commit

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+export default {
+    products: [{
+        _id: '1',
+        name: "Slim Shirt",
+        category: "Shirts",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: '2',
+        name: "Fit Shirt",
+        category: "Shirts",
+        image: "/images/d1.jpg",
+        price: 50,
+        brand: "Nike",
+        rating: 4.3,
+        numReviews: 10
+    },
+    {
+        _id: '3',
+        name: "Best Pants",
+        category: "Pants",
+        image: "/images/d1.jpg",
+        price: 70,
+        brand: "Nike",
+        rating: 4.6,
+        numReviews: 10
+    },
+    {
+        _id: '4',
+        name: "Cargo Pants",
+        category: "Pants",
+        image: "/images/d1.jpg",
+        price: 45,
+        brand: "Nike",
+        rating: 4.7,
+        numReviews: 5
+    }]
+}
